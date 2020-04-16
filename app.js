2bafec6a5ef779a542557f3f5133b5d3f94bbfab
Tomiwa Adedokun <140408024@live.unilag.edu.ng>
2020-04-16 17:51:20 +0100

    Middleware update

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const express = require('express');
 
 const app = express();
 
+/*
+// How to use middleware 
+app.use(function(req, res, next){
+  //console.log(Date.now());
+  req.name = 'Tomiwa Adedokun';
+  next();
+});
+*/
+
 //Index Route
 app.get('/', (req, res) => {
+  //console.log(req.name)
   res.send('INDEX');
 });
 
 // About Route
 app.get('/about', (req, res) => {
-  res.send('ABOUT');
+  res.send('ABOUT APP');
 });
 
 const port = 5000;
